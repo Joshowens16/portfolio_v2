7fd64da0b71c079528ec123a839f88a2d0e20998
Joshowens16 <joshowens16@gmail.com>
2023-03-14 00:51:57 -0700

    link tree working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import About from "./About";
-import reactLogo from "./assets/react.svg";
-import ComponentRouter from "./ComponentRouter";
-import Home from "./Home";
 import Nav from "./Nav";
-import Socials from "./socials/Socials";
-import "./socials/socials.css";
+import Socials from "./Socials";
+import "./app.css";
 function App() {
   return (
     <div className="home">
---
+++ src/Socials.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const socials: { devicon: string; link: string }[] = [
+  {
+    devicon: "devicon-linkedin-plain",
+    link: "https://www.linkedin.com/in/joshowens97/",
+  },
+  {
+    devicon: "devicon-github-original",
+    link: "https://github.com/joshowens16",
+  },
+  {
+    devicon: "devicon-twitter-original",
+    link: "",
+  },
+  {
+    devicon: "devicon-facebook-plain",
+    link: "",
+  },
+];
+const Socials = () => {
+  return (
+    <div className="flex flex-col text-white content-center py-2 px-2 text-3xl gap-4 bg-gray-700 rounded-lg fixed bottom-[30vh] left-[10px]">
+      {socials.map((link) => {
+        return (
+          <div className="flex justify-center hover:cursor-pointer">
+            <a href={link?.link} target="_blank">
+              <i className={link.devicon}></i>
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Socials;
