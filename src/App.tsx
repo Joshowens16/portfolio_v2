868bcf904e51777da2d076930d5d6e95be67c05f
Joshowens16 <joshowens16@gmail.com>
2023-04-05 23:17:39 -0700

    fixed console error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Nav from "./Nav";
 import Socials from "./Socials";
 import "./app.css";
+import SocialMediaBar from "react-social-media-bar";
 function App() {
   return (
     <div className="home">
