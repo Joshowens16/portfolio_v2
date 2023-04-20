3861dcae58d892123e9353b4e4fffe6892f6a0f4
Joshowens16 <joshowens16@gmail.com>
2023-04-20 09:28:24 -0700

    removed a project

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import projects from "./projectInfo";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Projects = () => {
   return (
